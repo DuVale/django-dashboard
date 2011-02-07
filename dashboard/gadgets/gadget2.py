0fc02cc6ff6f9efe5ba901f783729cb3e441fe88
ThomasWTurner <ThomasWTurner@gmail.com>
2011-02-07 19:43:51 +0000

    Added a checkbox option

--- dashboard/gadgets/gadget2.py
+++ dashboard/gadgets/gadget2.py
@@ -7,8 +7,9 @@ class Gadget:
     
     def gadget_info(self):
         fields = [{'title':'This is a test field','id':'test','type':'text','value':'test'},
-                  {'title':'List of countries','id':'test2','type':'choice',
-                                    'choices':[('0','UK'),('1','USA'),('2','France'),('3','Italy')],'value':'UK'}]
+                    {'title':'List of countries','id':'test2','type':'choice',
+                                    'choices':[('0','UK'),('1','USA'),('2','France'),('3','Italy')],'value':'UK'},
+                    {'title':'This is a test checkbox field','id':'chtest','type':'checkbox','value':'0'}]
         return {'name':'gadget2',
                 'title':'This is a gadget2',
                 'description':'This is gadget2',
